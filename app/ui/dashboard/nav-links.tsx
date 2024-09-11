12e9271b2364907bb2d55cf3b0477529c79e6f10
SurendharSSenthil <surendharsenthil257@gmail.com>
2024-09-11 18:33:07 +0530

    first commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,37 +1,44 @@
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from '@heroicons/react/24/outline';
+'use client'; // Should be at the top for client-side components
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+import Link from 'next/link'; // Correct import for Link
+import {
+	HomeIcon,
+	DocumentDuplicateIcon,
+	UserGroupIcon,
+} from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+	{ name: 'Home', href: '/dashboard', icon: HomeIcon },
+	{
+		name: 'Invoices',
+		href: '/dashboard/invoices',
+		icon: DocumentDuplicateIcon,
+	},
+	{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
-  return (
-    <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <a
-            key={link.name}
-            href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
-        );
-      })}
-    </>
-  );
+	const pathname = usePathname();
+
+	return (
+		<>
+			{links.map((link) => {
+				const LinkIcon = link.icon;
+				return (
+					<Link
+						key={link.name}
+						href={link.href}
+						className={clsx('flex items-center gap-2 p-3 text-sm font-medium', {
+							'bg-sky-100 text-blue-600': pathname === link.href,
+						})}
+					>
+						<LinkIcon className='w-6' />
+						<p>{link.name}</p>
+					</Link>
+				);
+			})}
+		</>
+	);
 }
